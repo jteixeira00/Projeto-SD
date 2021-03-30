f026ba287e12bc7a78333971d2362763f671f94d
diniscarvalho19 <49685992+diniscarvalho19@users.noreply.github.com>
2021-03-30 19:09:44 +0100

    pumba

--- src/RmiServer.java
+++ src/RmiServer.java
@@ -260,6 +260,7 @@ public class RmiServer extends UnicastRemoteObject implements RmiInterface {
     //to-do
     @Override
     public boolean criaMesaRMI(String departamento) throws RemoteException {
+
         return false;
     }
 
